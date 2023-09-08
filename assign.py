1717c2a427de331f22d56cf35c622a98d030f045
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-09-08 03:13:15 -0700

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,22 @@
+a = 10
+print(a)
+
+a = a+5
+print(a)
+a += 5  # same as concatenation
+print(a)
+
+a = a-5
+print(a)
+a -= 5  # same as concatenation
+print(a)
+
+a = a * 5
+print(a)
+a *= 5  # same as concatenation
+print(a)
+
+a = a / 5
+print(a)
+a /= 5  # same as concatenation
+print(a)
