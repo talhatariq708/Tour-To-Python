fa9cedbd2487dce46d606cf78dc81b37159d2a44
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-09-24 05:37:39 -0700

    Add files via upload

---
+++ bop.py
@@ -0,0 +1,11 @@
+x = 8
+y = 10
+print(bin(x))
+print(bin(y))
+print(bin(x & y), x & y)  # 1000 8
+print(bin(x | y), x | y)  # 1010 10
+print(bin(x ^ y), x ^ y)  # 0010 2
+
+# 1000 is the binary of 8
+# 1010 is the binary of 10
+# 0010 is the binary of 2
