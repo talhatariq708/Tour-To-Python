81bf07a53d2d4747d5e7121d22e8833364c1bfdd
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-09-26 04:53:12 -0700

    Add files via upload

---
+++ dttype.py
@@ -0,0 +1,17 @@
+a = 100
+print(a, type(a))  # int datatype
+b = 20.8
+print(b, type(b))  # float datatype
+c = 20 + 3j
+print(c, type(c))  # complex datatype
+d = [10, 200]
+d[1] = 20  # changeable datatype
+print(d, type(d))  # list datatype
+e = (10, "Hello World", 235)
+print(e, type(e))  # tupal datatype
+f = {
+    'courseName': "Python"
+}
+print(f, type(f))  # dictionary datatype
+g = {10, 20, "Hi", 10}
+print(g, type(g))  # set datatype
