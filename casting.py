4d603e9dc9291355d9faf02bc6f440c275b71698
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-10-03 04:56:13 -0700

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,15 @@
+a = input("Enter your value1:- ")
+b = input("Enter your value2:- ")  # input value will combine instead adding
+print(a + b)
+
+c = int(input("Enter your value:- "))
+d = int(input("Enter your value:- "))  # int will not add decimal number
+print(c + d)
+
+e = float(input("Enter your value:- "))
+f = float(input("Enter your value:- "))  # float will also add decimal also int
+print(e + f)
+
+g = eval(input("Enter your value:- "))
+h = eval(input("Enter your value:- "))  # eval will add both float and int
+print(g + h)
