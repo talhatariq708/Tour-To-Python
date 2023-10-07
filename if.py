8098db5ed3d22ab3dbe4bf565cb14a90181ed2af
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-10-07 09:56:03 -0700

    Add files via upload

---
+++ if.py
@@ -0,0 +1,10 @@
+# if elif else statement
+per = int(input("Enter the number:- "))
+if per >= 60:
+    print("First Div")
+elif per >= 45:
+    print("Second Div")
+elif per >= 35:
+    print("Third Div")
+else:
+    print("Fail")
