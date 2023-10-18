fda39472a04835907113b0c2be28910a9cae4ab9
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-10-18 05:47:27 -0700

    Add files via upload

---
+++ for.py
@@ -0,0 +1,12 @@
+range(5)  # from 1 to 5
+range(1, 5)  # from 1 to 4
+range(2, 10, 5)  # inc of 2 till 10
+
+for n in range(5):
+    print("Hello World")  # will print 5 times
+print()  # for 1 line space
+for n_1 in range(2, 5):
+    print("3 times ans")  # will print 3 times
+print()  # for 1 line space
+for n_2 in range(2, 10, 5):
+    print("2 times")  # will print 2 times
