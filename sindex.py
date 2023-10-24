4a0b2a9c33e7a0c686c4059c63ad22dc64ddcde4
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-10-24 06:31:21 -0700

    Add files via upload

---
+++ sindex.py
@@ -0,0 +1,6 @@
+a = "Hello World"
+print(a[-1::-1])  # will reverse the string
+b = "Hello My Name is Talha Tariq"
+print(b[5:8])  # will show specific index string
+c = "Hi there!"
+print(c[0:2])  # will show from zero to specific string
