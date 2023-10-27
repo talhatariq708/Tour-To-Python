a51e54106d3b3b1c8090e79289e0706b6c7bbca1
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-10-27 03:06:15 -0700

    Add files via upload

---
+++ strfun.py
@@ -0,0 +1,15 @@
+a = "Hello World"
+b = a.lower()  # will lower all the case
+print(b)
+
+c = "Talha Tariq"
+d = c.upper()  # will upper all the case
+print(d)
+
+e = "Python is A coDinG LanGuagE"
+f = e.title()  # will upper the first letter of every sentence
+print(f)
+
+g = "it is USE FOR BACkend TechnOlOgY"
+h = g.capitalize()  # only upper the first character of all the sentence
+print(h)
