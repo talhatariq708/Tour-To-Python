6a3caf2966f0f852358224311e5810dfbd6895d1
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-11-16 04:39:32 -0800

    Add files via upload

---
+++ strfun1.py
@@ -0,0 +1,19 @@
+a = "Hello World"
+b = a.find("e")  # will find the index
+print(b)
+
+c = "Talha"
+d = c.index("l")  # will find the index (same)
+print(d)
+
+e = "Tariq"
+f = e.isalpha()  # if there's an alphabet
+print(f)
+
+g = "111"
+h = g.isdigit()  # if there is an digit
+print(h)
+
+i = "ABC123"
+j = i.isalnum()
+print(j)  # if there is both digit and alphabet
