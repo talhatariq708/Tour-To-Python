77de3b2792a8d9eb6c1c7b62bf1b98bc97831f0e
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-12-05 07:42:43 -0800

    Add files via upload

---
+++ comp.py
@@ -0,0 +1,14 @@
+l_1 = []
+for n in range(1, 101):  # 101 because the counting will start from 0
+    print(n)  # this will show the numbers from 1 to 100
+    l_1.append(n)
+
+print(l_1)  # this will show the listing value from 1 to 100
+
+
+l_2 = []
+for a in range(1, 51):
+    print(a)
+    l_2.append(a)
+
+print(l_2)
