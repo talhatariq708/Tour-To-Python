252a4134458232a4a7521b89584038fdbcabf204
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2023-12-05 07:43:36 -0800

    Add files via upload

---
+++ listfund.py
@@ -0,0 +1,18 @@
+# in this class we will do
+# sort, reverse, max, min, and clear functions
+
+l_1 = [10, 20, 40, 50, 80, 90]
+l_2 = [30, 10, 20, 50, 100, 101, 200]
+w = l_1.clear  # this will clear the list
+w_1 = l_1.count(20)  # this will count the var in list
+w_2 = max(l_1)  # this will show the max value in list
+w_3 = min(l_1)  # this will show the min value in list
+l_1.sort  # this will sort the value from ascending to descending
+l_1.reverse
+print(w)
+print(w_1)
+print(w_2)
+print(w_3)
+# print(w_4)
+print(l_1)
+print(l_2)  # this will sort the value from descending to ascending
