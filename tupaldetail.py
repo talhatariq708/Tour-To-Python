50cb1f7c1134446498ab002199d6761410200407
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2024-01-31 06:19:38 -0800

    Add files via upload

---
+++ tupaldetail.py
@@ -0,0 +1,14 @@
+# tupal in detail
+
+a = (10, 10, 10, 20, 30, 40, 50)
+print(type(a))
+b = a.count(10)  # this will count how many 10 are there in tupal
+print(b)
+c = max(a)  # this will print the max value in a (tupal)
+print(c)
+d = min(a)
+print(d)  # this will print the min value in a (tupal)
+e = sum(a)
+print(e)  # this will sum the whole tupal in a
+f = a.index(30)
+print(f)  # this will find the index of 30 in tupal
