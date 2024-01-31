87f0486b7d8eb652c3e983bbdf1e4fc24733753a
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2024-01-31 06:14:05 -0800

    Add files via upload

---
+++ dictdetail.py
@@ -0,0 +1,14 @@
+# dictionary operator
+
+a = {
+    'name': 'Talha Tariq',
+    'class': 'intermediate',
+    'programming language': 'python',
+    'session': 27
+}
+print(a)
+for n in a:
+    print("")  # this will make one line gap
+    print(n)  # this will print only keys
+    print(a[n])  # this will print key's statements
+    print("")  # this will make on line gap
