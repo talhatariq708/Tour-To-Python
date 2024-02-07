bfd2626049cca21f0f7c07c4835d94e7e6ab6033
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2024-02-07 11:50:19 -0800

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,9 @@
+# sets
+
+s = (10, 20, 30, 40)
+a = set(s)  # random output will generate in sets
+print(a)
+print("")
+l = [50, 80, 110, 20]
+l_1 = set(l)  # will change from list to set
+print(l_1)
