1b374b5d3a54dfca45c1c97aa0d8845efb56920d
Talha Tariq <134872539+talhatariq708@users.noreply.github.com>
2024-07-01 00:31:16 +0500

    Add files via upload

---
+++ morpp.py
@@ -0,0 +1,11 @@
+class Student:
+    def Student(self):
+        print("Hello")
+class IPP(Student):
+    def IPP(Student):
+        print("Hello World!")
+class Marks(IPP):
+    def Marks(IPP):
+        print("This will be printed!")
+obj = Marks()
+obj.Marks()
